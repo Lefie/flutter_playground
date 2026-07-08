448d3dc6d6257e84f065c580ca34f606f1455486
Lefie <qiwenruan11@gmail.com>
2026-07-08 14:53:55 -0400

    Add an example

--- lib/main.dart
+++ lib/main.dart
@@ -24,18 +24,149 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF083F68)),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color(0xFF083F68),
+          titleTextStyle: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 25,
+              color: Color(0xFF61B7E8),
+          )
+        ),
+        scaffoldBackgroundColor: Color(0xFF61B7E8),
         useMaterial3: true,
       ),
-      initialRoute: '/',
+      home: AccountCreationScreen(),
+
+    );
+  }
+}
+
+class Account {
+  String? username;
+  String? email;
+  String? password;
+
+  Account({this.username, this.email, this.password});
+}
+
+
+class AccountCreationScreen extends StatefulWidget {
+  @override
+  _AccountCreationScreenState createState() => _AccountCreationScreenState();
+}
+
+class _AccountCreationScreenState extends State<AccountCreationScreen> {
+  final _formKey = GlobalKey<FormState>();
+  Account acc = Account();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Registration Form")),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 130,),
+              SizedBox(
+                width: 200,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Username"
+                  ),
+                  onSaved: (String? value){
+                          setState(() {
+                            acc.username = value;
+                          });
+                  },
+                ) ,
+              ),
+              SizedBox(
+                width: 200,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Email"
+                  ),
+                  onSaved: (String? value){
+                    setState(() {
+                      acc.email = value;
+                    });
+
+                  },
+                ) ,
+              ),
+              SizedBox(
+                width: 200,
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      labelText: "Password",
+                  ),
+                  onSaved: (String? value){
+                    setState(() {
+                      acc.password = value;
+                    });
+                  },
+                ) ,
+              ),
+              SizedBox(height: 20,),
+              Align(alignment: Alignment.center,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                      backgroundColor: Color(0xFF083F68),
+                      foregroundColor: Color(0xFF61B7E8)
+                  ),
+                  onPressed: (){
+                    _formKey.currentState!.save();
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen(acc: acc)));
+                  },
+                  child: Text("Register"), ),)
+
+              // Form fields go here
+            ],
+          ),
+        ),
+      ),
+    );
+
+  }
+}
+
+
+class HomeScreen extends StatelessWidget{
+  HomeScreen({super.key, this.acc});
+
+  Account? acc;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Home")),
+      body: Center(child: Column(
+        children: [
+          Text("Welcome, ${acc?.username ??  "Winnie"}"),
+          SizedBox(height:30),
+          ElevatedButton(onPressed: (){
+            Navigator.pop(context);
+          }, child: Text("Return"))
+
+        ],
+      ),),
+    );
+  }
+}
+
+/*
+// advanced example
+initialRoute: '/',
       routes: {
         '/': (context) => HomeScreen() ,
         '/contacts': (context) => ContactsListScreen(contacts:contacts),
         '/advanced_example': (context) => ScreenOne(),
-      },
-    );
-  }
-}
+},
+ */
 
 
 
